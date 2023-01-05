2e12cd8ae985d40bf558c3327e065ee1b4d293c7
Turtle.ca <sleepingturtle294@gmail.com>
2023-01-04 19:04:22 -0700

    more creppe

--- Battleship.py
+++ Battleship.py
@@ -3,15 +3,29 @@
 # 1/4/2023 
 #start the board functions and possibly the visuals?
 
+#create an empty list of specified dimentions
+#int, int
 def createEmptyBoard(columns,rows):
-    emptyList = []
-    for i in range(columns):
-        emptyList.append([])
-        for j in range(rows):
-            emptyList[i].append([])
+    emptyList = [" "]
+    for i in range(rows):
+        emptyList.append([" "])
+        for j in range(columns):
+            emptyList[i].append([" "])
     return(emptyList)
+#list
 
-def start():
-    print(createEmptyBoard(10,10))
+#places ships in specified locations on a board
+#int, string, list
+def ShipCreation(shipClass,position,board): #position string in format "e40" "row, column, direction". shipClass either int or ship name
+    if shipClass == 0:
+        shipLetter = "P"
 
-start()
+#edited list
+
+#Main function
+#n/a
+def main():
+    pass
+#n/a
+
+main()
